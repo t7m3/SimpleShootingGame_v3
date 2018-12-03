372095db9b8935b4e460897135618ebf11ff1608
t7m3 <toda7.mk3@gmail.com>
2018-12-03 20:45:08 +0900

    2018-12-03 ５回目のコミット （敵、自機、弾 の初期位置を設定した）

--- app/src/main/java/com/example/user200/simpleshootinggame_v3/MainActivity.kt
+++ app/src/main/java/com/example/user200/simpleshootinggame_v3/MainActivity.kt
@@ -21,6 +21,18 @@ class MainActivity : AppCompatActivity() {
         screenWidth = dMetrics.widthPixels  //スクリーンの幅を取得
         screenHeight = dMetrics.heightPixels  //スクリーンの高さを取得
 
+        // imageViewEnemy の初期位置の設定
+        imageViewEnemy.x = 50F
+        imageViewEnemy.y = screenHeight.toFloat() * 0.2F
+
+        // imageViewPlayer の初期位置の設定
+        imageViewPlayer.x = 50F
+        imageViewPlayer.y = screenHeight.toFloat() * 0.6F
+
+        // imageViewBullet の初期位置の設定
+        imageViewBullet.x = 50F
+        imageViewBullet.y = screenHeight.toFloat() * 0.4F
+
         // タイマのインスタンスの生成
         val timer = MyCountDownTimer(5 * 60 * 1000, 100)
         timerText.text = "5:00"
