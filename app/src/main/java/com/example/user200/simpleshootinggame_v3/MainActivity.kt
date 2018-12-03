3e32ade84f17382d6324846279a281cef280534c
t7m3 <toda7.mk3@gmail.com>
2018-12-03 20:38:33 +0900

    2018-12-03 ４回目のコミット （スクリーンの幅と高さの取得を追加した）

--- app/src/main/java/com/example/user200/simpleshootinggame_v3/MainActivity.kt
+++ app/src/main/java/com/example/user200/simpleshootinggame_v3/MainActivity.kt
@@ -3,13 +3,23 @@ package com.example.user200.simpleshootinggame_v3
 import android.os.Bundle
 import android.os.CountDownTimer
 import android.support.v7.app.AppCompatActivity
+import android.util.DisplayMetrics
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    private var screenWidth = 0  //スクリーンの幅を格納する変数の宣言
+    private var screenHeight = 0   //スクリーンの高さ格納する変数の宣言
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // スクリーンの幅と高さを取得する
+        val dMetrics = DisplayMetrics()  //DisplayMetrics のインスタンスを生成する
+        windowManager.defaultDisplay.getMetrics(dMetrics)  //スクリーンサイズを取得しているらしい
+        screenWidth = dMetrics.widthPixels  //スクリーンの幅を取得
+        screenHeight = dMetrics.heightPixels  //スクリーンの高さを取得
 
         // タイマのインスタンスの生成
         val timer = MyCountDownTimer(5 * 60 * 1000, 100)
