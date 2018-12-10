0d3cab65f1e6aee04f1217af1c9e4ab6e02a165b
NJC-T1 <toda7.mk3@gmail.com>
2018-12-10 10:22:21 +0900

    2018-12-10 １回目のコミット（クラスBulletの中の変数名を一部変更した。MainActivityの中をクラスBulletで統一した。）

--- app/src/main/java/com/example/user200/simpleshootinggame_v3/Bullet.kt
+++ app/src/main/java/com/example/user200/simpleshootinggame_v3/Bullet.kt
@@ -2,21 +2,22 @@ package com.example.user200.simpleshootinggame_v3
 
 import android.widget.ImageView
 
-public class Bullet(imageView: ImageView,height:Int) {
+public class Bullet(image:ImageView, height:Int) {
 
-    val image = imageView
-    val screenHeight = height
-    var status = "stop"
+    public val imageView = image
+    private val screenHeight = height
+
+    public var status = "stop"
 
     public fun move(y:Int){
 
-        image.y = image.y - y
+        imageView.y = imageView.y - y
 
-        if(image.y <= 0){  //画面の上端になったら
+        if(imageView.y <= 0){  //画面の上端になったら
             //image.visibility = View.INVISIBLE  // 非表示にする。
-            this.status = "stop"
-            image.x = 0F  // 位置を左下にする
-            image.y = screenHeight.toFloat() * 0.7F  // 位置を左下にする
+            status = "stop"
+            imageView.x = 0F  // 位置を左下にする
+            imageView.y = screenHeight.toFloat() * 0.7F  // 位置を左下にする
         }
 
     }
